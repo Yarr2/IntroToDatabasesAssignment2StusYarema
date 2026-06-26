047845ba4579141d905d0427e5c22edde22bfbf7
Yarema Stus <151072658+Yarr2@users.noreply.github.com>
2026-06-26 23:50:33 +0300

    Optimize customer spending queries and indexing
    Refactored SQL queries to optimize customer spending analysis by using CTEs and indexing. Improved performance by reducing subqueries and applying filters more efficiently.

--- OptimizationQuerys.sql
+++ OptimizationQuerys.sql
@@ -73,6 +73,7 @@ select
  * по перше я виніс в CTE фільтрацію order яка спільна для 4 підзапитів минулої query
  * далі я створив сustomer_spending який рахує сумарну кількість потрачених грошей
  * далі я разом порангував ці відфільтровані дані по зростанню і спаданню щоб швидко найти мінімум і максимум
+ * ця версія при explain analyze показує seq scan бо restriction не дуже жорсткі і БД вважає що seq scan буде продуктивнішим EA_1
  * */
 explain analyze
 with filtered_orders as (
@@ -107,6 +108,45 @@ select
     max(concat(customer_name, ': $', total_spent)) filter (where min_rn = 1) as min_spent_customer,
     max(concat(customer_name, ': $', total_spent)) filter (where max_rn = 1) as max_spent_customer
 from ranked_customers;
+
+/* ========================================
+ * тепер версія з жорсткішими рамками для якої БД використовує індекси  EA_2
+ */
+
+explain analyze
+with filtered_orders as (
+    select 
+        o.order_id,
+        c.name as customer_name,
+        p.price,
+        o.quantity
+    from orders o
+    join products p on o.product_id = p.product_id
+    join customers c on o.customer_id = c.customer_id
+    where o.order_date > date '2024-11-01'
+      and o.status = 'Delivered'
+      and c.status in ('premium')
+),
+customer_spending as (
+    select
+        customer_name,
+        sum(price * quantity) as total_spent
+    from filtered_orders
+    group by customer_name
+),
+ranked_customers as (
+    select 
+        customer_name,
+        total_spent,
+        row_number() over (order by total_spent asc, customer_name asc) as min_rn,
+        row_number() over (order by total_spent desc, customer_name asc) as max_rn
+    from customer_spending
+)
+select
+    max(concat(customer_name, ': $', total_spent)) filter (where min_rn = 1) as min_spent_customer,
+    max(concat(customer_name, ': $', total_spent)) filter (where max_rn = 1) as max_spent_customer
+from ranked_customers;
+
 
 
 set enable_indexscan = on;
